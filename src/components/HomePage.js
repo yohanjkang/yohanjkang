c038b664469f46a5705dbd3e9a761e7569673eda
Yohan Kang <yohankan@usc.edu>
2022-02-07 17:42:40 -0800

    'website'

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomePage.css";
+
+export default function HomePage() {
+  return (
+    <div className="home-page">
+      <div className="home-container">
+        <h1>Hello! My name is Yohan.</h1>
+        <h2>I'm a software developer.</h2>
+      </div>
+    </div>
+  );
+}
