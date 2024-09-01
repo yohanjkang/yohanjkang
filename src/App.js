b6895e4621f1f58718b47f8531901848746d84e0
yohanjkang <yohankang0425@gmail.com>
2024-09-01 04:24:22 -0700

    Initial commit from local file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+    </>
+  );
+}
+
+export default App;
